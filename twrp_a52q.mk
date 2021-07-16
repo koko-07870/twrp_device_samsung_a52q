838a2559e9a35787384ec58afb1b90609d96b75e
Daniel Rosenberg <drosen@google.com>
2021-07-16 12:52:43 +0200

    a52q: Enable casefolding support on userdata
    This enables casefolding, which is required for removal of sdcardfs.
    
    Test: Wipe data and boot. lsattr /data/media, check for +F
    Bug: 138322712
    Change-Id: I020aa6bb9c121e7bda83c07bc9fb37dcfa26702c

--- twrp_a52q.mk
+++ twrp_a52q.mk
@@ -21,6 +21,9 @@ $(call inherit-product, device/samsung/a52q/device.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a52q
 PRODUCT_NAME := twrp_a52q
